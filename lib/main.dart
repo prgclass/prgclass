86ef0588c3f1441566b71f84a5df5aef9614c78a
sdkarimi <saeedkarimi.developer@gmail.com>
2024-05-13 12:10:43 +0430

    first COMMIT

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,209 @@
+
+import 'package:blackmovies/ui/Search.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:blackmovies/ui/Movies.dart';
+import 'package:blackmovies/ui/Home.dart';
+import 'package:blackmovies/ui/Series.dart';
+import 'package:blackmovies/ui/splashscreen.dart';
 void main() {
-  runApp(const MyApp());
-}
+  runApp(MaterialApp(
 
+    initialRoute: '/splash',
+    routes: {
+      '/splash': (context) => const SplashScreen(),
+      '/home': (context) => const MyApp(),
+    },
+  ));
+}
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  final Color colorAppBar = const Color(0xff161616);
+  final defaultTextStyle = const TextStyle(fontFamily: 'IranSans');
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Black Movie',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        textTheme: TextTheme(
+            bodyMedium: defaultTextStyle.copyWith(fontWeight: FontWeight.bold),
+            titleLarge: defaultTextStyle.copyWith(fontWeight: FontWeight.bold)),
+
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          selectedLabelStyle: TextStyle(shadows: <Shadow>[
+            Shadow(
+              color: Colors.orange.shade400,
+              blurRadius: 20,
+            )
+          ]),
+          selectedItemColor: Colors.orange.shade800,
+          unselectedItemColor: Colors.orange,
+          backgroundColor: colorAppBar,
+          selectedIconTheme: IconThemeData(color: Colors.orange.shade800,shadows: <Shadow>[
+            Shadow(
+              color: Colors.orange.shade400,
+              blurRadius: 30,
+            )
+          ]),
+          unselectedIconTheme: IconThemeData(color: Colors.orange.shade500),
+        ),
+        appBarTheme: AppBarTheme(
+          titleSpacing: 2,
+          color: colorAppBar,
+        ),
+        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Directionality(
+          textDirection: TextDirection.rtl,
+          child: HomePage()),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+  List<Widget> listOptions = <Widget>[Home(), Movies(), Series()];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.white10,
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        leading: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+          child: IconButton(
+            color: Colors.orange.shade500,
+            icon: Icon(CupertinoIcons.bars,
+                size: MediaQuery.of(context).size.width * 0.1),
+            onPressed: () {
+
+            },
+          ),
+        ),
+        actions:[
+          Padding(
+          padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+          child: IconButton(
+            color: Colors.orange.shade500,
+            icon: Icon(CupertinoIcons.search_circle_fill,
+                size: MediaQuery.of(context).size.width * 0.09),
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+
+                  builder: (context) => const SearchPage())
+              );
+
+            },
+          ),
+        ),],
+        title: const Center(
+            child: Text("بلک مووی", style: TextStyle(color: Colors.orange))),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      //==============================(Navbar)==================================
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.fromLTRB(8, 8, 8, 6),
+        child: ClipRRect(
+          borderRadius: const BorderRadius.only(
+              bottomLeft: Radius.circular(30),
+              bottomRight: Radius.circular(30),
+              topLeft: Radius.circular(30),
+              topRight: Radius.circular(30)),
+          child: BottomNavigationBar(
+            currentIndex: _currentIndex,
+            onTap: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            items: const [
+              BottomNavigationBarItem(
+                  icon: Icon(CupertinoIcons.home), label: "خانه"),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.movie_creation_outlined), label: "فیلم ها"),
+              BottomNavigationBarItem(
+                  icon: Icon(CupertinoIcons.play_rectangle), label: "سریال ها"),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      //============================(END /Navbar)===============================
+      body: listOptions.elementAt(_currentIndex),
     );
   }
 }
+
+
+//
+// class SplashScreen extends StatefulWidget {
+//   const SplashScreen({Key? key}) : super(key: key);
+//
+//   @override
+//   _SplashScreenState createState() => _SplashScreenState();
+// }
+//
+// class _SplashScreenState extends State<SplashScreen> {
+//   late Timer _timer;
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     _timer = Timer(const Duration(seconds: 5), () {
+//       Navigator.of(context).pushReplacementNamed('/home');
+//     });
+//   }
+//
+//   @override
+//   void dispose() {
+//     _timer.cancel(); // لغو تایمر در صورت خروج از ویجت
+//     super.dispose();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.black,
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             SizedBox(
+//               height: 160,
+//               width: 160,
+//               child: Image.asset(
+//                 'assets/img/logo/logo.jpg',
+//                 fit: BoxFit.cover,
+//               ),
+//             ),
+//             const SizedBox(height: 24),
+//             const Text(
+//               'بلک مووی',
+//               style: TextStyle(
+//                 color: Colors.orange,
+//                 fontSize: 24,
+//                 fontWeight: FontWeight.bold,
+//               ),
+//             ),
+//             const SizedBox(height: 8),
+//              CircularProgressIndicator(
+//               color: Colors.orange.shade400,
+//               backgroundColor: Colors.yellowAccent.shade700,
+//               // valueColor:  AlwaysStoppedAnimation<Color>(Colors.yellowAccent.shade200),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
